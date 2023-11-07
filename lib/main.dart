bdd66a00b27afacfb9fcb386c63b8b5713a3beef
alexmalder <vnmntn@mail.ru>
2023-11-07 08:14:10 +0300

    mobx with codegen

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:mobx/mobx.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:videos/routes/app_routes.dart';
+import 'package:videos/counter.dart';
 
-part 'counter.g.dart';
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = dotenv.env['SENTRY_DSN'];
+      options.tracesSampleRate = 1.0;
+      options.debug = true;
+      options.sendDefaultPii = true;
+      options.reportSilentFlutterErrors = true;
+      options.attachScreenshot = true;
+      options.screenshotQuality = SentryScreenshotQuality.low;
+      options.attachViewHierarchy = true;
+      options.considerInAppFramesByDefault = false;
+      options.attachThreads = true;
+      options.enableWindowMetricBreadcrumbs = true;
+      options.environment = "production";
+    },
+    appRunner: () => runApp(const CounterExample()),
+  );
+}
 
-class Counter = CounterBase with _$Counter;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-abstract class CounterBase with Store {
-  @observable
-  int value = 0;
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routeInformationProvider: AppRouter.router.routeInformationProvider,
+      routeInformationParser: AppRouter.router.routeInformationParser,
+      routerDelegate: AppRouter.router.routerDelegate,
+      title: 'Go Router',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.black,
+          colorScheme: const ColorScheme.dark(),
+      ),
+    );
 
-  @action
-  void increment() {
-    value++;
+    /*
+    return MaterialApp(
+      title: 'Mobile',
+      theme: ThemeData.dark(),
+      home: const TodoListPage(),
+    );
+    */
   }
 }
 
-class CounterExample extends StatefulWidget {
-  const CounterExample({Key? key}) : super(key: key);
-
-  @override
-  _CounterExampleState createState() => _CounterExampleState();
-}
-
-class _CounterExampleState extends State<CounterExample> {
-  final _counter = Counter();
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: const Text('Counter'),
-    ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text(
-            'You have pushed the button this many times:',
-          ),
-          Observer(
-              builder: (_) => Text(
-                '${_counter.value}',
-                style: const TextStyle(fontSize: 20),
-              )),
-        ],
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: _counter.increment,
-      tooltip: 'Increment',
-      child: const Icon(Icons.add),
-    ),
-  );
-}
